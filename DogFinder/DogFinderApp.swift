dae10d34fba6c028de0f0fa07d203be13d7e2132
AnastasiaNim <nastya.nimchenko@yandex.ru>
2025-03-19 20:19:58 +0300

    Initial Commit

---
+++ DogFinder/DogFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogFinderApp.swift
+//  DogFinder
+//
+//  Created by Anastasia N.  on 19.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct DogFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
