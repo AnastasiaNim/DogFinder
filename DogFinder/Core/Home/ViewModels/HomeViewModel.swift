fe2216e3072a9c0f34185c67693a4cf74ad4a2ff
AnastasiaNim <nastya.nimchenko@yandex.ru>
2025-04-08 00:19:22 +0300

    feat: add HomeViewModel with state and loading logic

--- DogFinder/Core/Home/ViewModels/HomeViewModel.swift
+++ DogFinder/Core/Home/ViewModels/HomeViewModel.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+import Combine
+
+
+class HomeViewModel: ObservableObject {
+    
+    @Published var breeds: [Breed] = []
+    @Published var isLoading: Bool = false
+    
+    private var currentPage: Int = 0
+    private var cancellable: AnyCancellable?
+    
+    deinit {
+        cancellable?.cancel()
+    }
+    
+    func fetchBreeds(for page: Int = 0, for size: Int = 10) {
+        
+        self.isLoading = true
+        
+        cancellable = DogDataService.list(page: page, size: size)
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedBreeds) in
+                self?.isLoading = false
+                self?.breeds.append(contentsOf: returnedBreeds)
+            })
+        
+    }
+    
+    func fetchFirstPage() {
+        guard breeds.isEmpty  else { return }
+                currentPage = 0
+                fetchBreeds(for: currentPage)
+            }
+
+    
+    func needsFetchNextPage(id: Int) -> Bool {
+        return breeds.last?.id == id
+    }
+    
+    func fetchNextPage() {
+        guard !isLoading else { return }
+        currentPage += 1
+        fetchBreeds(for: currentPage)
+    }
+    
+}
+
+
+
+
+
+
