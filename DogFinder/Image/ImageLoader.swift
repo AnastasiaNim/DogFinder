9a8b1c7e62430bc571a249f2760bf146405b05b3
AnastasiaNim <nastya.nimchenko@yandex.ru>
2025-04-08 00:23:36 +0300

    chore: add image loading library for remote image support

--- DogFinder/Image/ImageLoader.swift
+++ DogFinder/Image/ImageLoader.swift
@@ -6,13 +6,55 @@
 //
 
 import SwiftUI
+import NukeUI
 
+// пакет https://github.com/kean/Nuke
+// выбрать при добавлении в xcode после установки пакет NukeUI
 struct ImageLoader: View {
+    let url: URL?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Rectangle()
+            .opacity(0)
+            .overlay {
+                NukeImageLoader(url: url)
+                    .allowsHitTesting(false)
+            }
+            .clipped()
     }
 }
 
-#Preview {
-    ImageLoader()
+fileprivate struct NukeImageLoader: View {
+    @State private var showError: Bool = false
+    let url: URL?
+    var body: some View {
+ 
+        LazyImage(url: url) { state in
+            if let image = state.image {
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            } else if state.error != nil {
+                ZStack {
+                    Color.red
+                    Text("Error loading image")// Indicates an error
+                }
+            } else {
+                Color.gray.opacity(0.1)
+            }
+        }
+    }
 }
+
+
+#Preview {
+    VStack {
+        
+        ImageLoader(url: nil)
+        ImageLoader(url: .init(string: "https://fastly.picsum.photos/id/16/367/267.jpg?hmac=ZyyuET1a6X-Ym6MXK8OyHrdWFJiLI4To0iYLTlyrD-0")!)
+        ImageLoader(url: .init(string:"https://fastly.WRONGURL")!)
+        
+    }
+    
+}
+
+
