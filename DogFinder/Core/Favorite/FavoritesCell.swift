8dfe81008457502a66ced4dae665a5d21a7bc4a2
AnastasiaNim <nastya.nimchenko@yandex.ru>
2025-04-29 21:50:06 +0300

    feat: add a Favorites screen

---
+++ DogFinder/Core/Favorite/FavoritesCell.swift
@@ -0,0 +1,74 @@
+//
+//  FavoritesCell.swift
+//  DogFinder
+//
+//  Created by Anastasia N.  on 23.04.2025.
+//
+
+import SwiftUI
+
+struct FavoritesCell: View {
+    let dog: Breed
+    let onRemove: () -> Void
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            imageBreed
+            HStack(spacing: 0) {
+                nameBreed
+                Spacer()
+                
+                deleteButton
+            }
+            .padding(.top, 8)
+        }
+        .padding(10)
+        .background(Color.white)
+        .cornerRadius(20)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.secondary.opacity(0.2).ignoresSafeArea()
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 160))]) {
+                FavoritesCell(dog: Breed.mockBreeds.first!) {
+                    print("Remove dog with id")
+                }
+                .frame(height: 200)
+            }
+            .padding()
+        }
+    }
+}
+extension FavoritesCell {
+    
+    private var imageBreed: some View {
+        ImageLoader(url: dog.imageURL)
+            .cornerRadius(20)
+    }
+    
+    private var nameBreed: some View {
+        Text(dog.name)
+            .font(.leckerliOne(size: 16))
+            .foregroundStyle(Color.black.opacity(0.7))
+            .lineLimit(1)
+    }
+    
+    private var deleteButton: some View {
+        Button {
+            onRemove()
+        } label: {
+            Image(systemName: "trash")
+                .font(.system(size: 15))
+                .padding(.horizontal, 10)
+                .padding(.vertical, 5)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.accentColor, lineWidth: 1)
+                )
+        }
+        
+    }
+}
--- DogFinder/Core/Favorite/FavoritesScreen.swift
+++ DogFinder/Core/Favorite/FavoritesScreen.swift
@@ -4,15 +4,114 @@
 //
 //  Created by Anastasia N.  on 26.03.2025.
 //
-
 import SwiftUI
 
 struct FavoritesScreen: View {
+    
+    @EnvironmentObject var favoriteDogs: BreedFavoritesManager
+    @State private var showAlert: Bool = false
+    @State var selectedBreed: Breed? = nil
+    
+    private let columns = [
+        GridItem(.adaptive(minimum: 150), spacing: 0)
+    ]
+    
     var body: some View {
-        Text("")
+        ZStack {
+            Color.secondary.opacity(0.2).ignoresSafeArea()
+            
+            if favoriteDogs.breeds.isEmpty {
+                emptyFavoritesView
+            } else {
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 5) {
+                        headerSection
+                        sectionFavoritesBreeds
+                    }
+                    .padding(15)
+                    .animation(.easeInOut, value: favoriteDogs.breeds)
+                    
+                    .alert(isPresented: $showAlert, content: deletionAlert)
+                }
+            }
+        }
     }
 }
 
 #Preview {
     FavoritesScreen()
+        .environmentObject(BreedFavoritesManager())
 }
+
+extension FavoritesScreen {
+    
+    
+    private var emptyFavoritesView: some View {
+        VStack(spacing: 10) {
+            
+            Text("You don't have any breeds in your favorites yet.")
+                .foregroundStyle(Color.accent)
+            Text("🐩")
+        }
+        .font(.system(size: 20))
+        .multilineTextAlignment(.center)
+    }
+    
+    private var headerSection: some View {
+        Group {
+            Text("Favorites")
+                .font(.leckerliOne(size: 35))
+            
+            Text("Did you find the breed you like? Add this breed to your favorites."
+                .attributed(highlights: ["Add this breed"]))
+            .font(.system(size: 20))
+            .foregroundStyle(.secondary)
+        }
+    }
+    private var sectionFavoritesBreeds: some View {
+        LazyVGrid(columns: columns, spacing: 0) {
+            ForEach(favoriteDogs.breeds) { breed in
+                NavigationLink(value: FavoritesLink.details(breed)) {
+                    FavoritesCell(
+                        dog: breed,
+                        onRemove: { 
+                            selectedBreed = breed
+                            showAlert = true
+                        }
+                    )
+                    .frame(height: 250)
+                    .padding(5)
+                }
+                
+            }
+        }
+        
+    }
+    
+    private var deletionAlert: () -> Alert {
+        {
+            Alert(
+                title: Text("Do you really want to delete from favorites \(selectedBreed?.name ?? "")?"),
+                message: Text(""),
+                primaryButton: .default(
+                    Text("No, I can't. Too cute!👹"),
+                    action: {
+                        showAlert = false
+                        selectedBreed = nil
+                    }),
+                secondaryButton: .destructive(
+                    Text("Delete!🧹"),
+                    action: {
+                        if let id = selectedBreed?.id {
+                            favoriteDogs.removeBreed(id)
+                        }
+                        showAlert = false
+                        selectedBreed = nil
+                    }
+                )
+            )
+        }
+    }
+}
+
+
